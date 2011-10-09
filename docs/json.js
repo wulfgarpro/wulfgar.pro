fbf6e8c0ccc80affd9cce64bc04d4f0c4adec225
James Fraser <wulfgar.pro@gmail.com>
2011-10-09 22:27:51 +1030

    added docs dir for project docs

---
+++ docs/json.js
@@ -0,0 +1,9 @@
+{ 
+  wulfgarpro: {
+    name: "James Fraser",
+    dob: new Date("Apr 16 1987"),
+    location: ".au",
+    occupation: "Software Developer",
+    email: "wulfgar.pro@gmail.com"
+  } 
+}
