211e49a627f50eb3023478d5710be6fb3af3ecc3
James Fraser <wulfgar.pro@gmail.com>
2011-10-09 22:30:59 +1030

    removed remnant json file

--- docs/json.js
+++
@@ -1,9 +0,0 @@
-{ 
-  wulfgarpro: {
-    name: "James Fraser",
-    dob: new Date("Apr 16 1987"),
-    location: ".au",
-    occupation: "Software Developer",
-    email: "wulfgar.pro@gmail.com"
-  } 
-}
